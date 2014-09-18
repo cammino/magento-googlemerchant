9014ab264e4dcd62dbfbb34edac10b4ce92161bb
Marcelo Ferracioli <marcelo@cammino.com.br>
2014-09-18 17:01:21 -0300

    adiciona opcao vazia

--- Model/Source/Category.php
+++ Model/Source/Category.php
@@ -6,6 +6,9 @@ class Cammino_Googlemerchant_Model_Source_Category extends Mage_Eav_Model_Entity
         if (!$this->_options) {
             $this->_options = Mage::getResourceModel('googlemerchant/category_collection')->load()->toOptionArray();
         }
+
+        array_unshift($this->_options, array("value" => "", "label" => Mage::helper('catalog')->__('-- Please Select --')));
+
         return $this->_options;
     }
 
