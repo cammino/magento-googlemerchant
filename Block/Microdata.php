c7af1a98af503a6e6186df8928a9e2ba1ab22858
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2015-10-23 16:37:37 -0200

    Atualiza verificacao de data no bloco da microdata

--- Block/Microdata.php
+++ Block/Microdata.php
@@ -44,7 +44,7 @@ class Cammino_Googlemerchant_Block_Microdata extends Mage_Core_Block_Template
 
             $price = $product->getPrice();
             
-            if ($product->getSpecialPrice() > 0 && ($now >= $product->getSpecialFromDate() && ( ($product->getSpecialToDate() != "") || $now <= $product->getSpecialToDate()))) {
+            if ($product->getSpecialPrice() > 0 && ($now >= $product->getSpecialFromDate() && ( ($product->getSpecialToDate() == "") || $now <= $product->getSpecialToDate()))) {
                 $price = $product->getSpecialPrice();
             }
 
